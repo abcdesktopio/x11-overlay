0ecb5b861c44ed2571ed1f80123f6c9860aa32d8
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-09-16 11:57:10 +0200

    docker-entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+X11SOCKET=/tmp/.X11-unix/X0
+echo "waiting for socket $X11SOCKET"
+while [ ! -S $X11SOCKET ]; do
+	echo -n '.'
+	sleep  0.1
+done
+echo "$ABCDESKTOP_USERNAME\n$ABCDESKTOP_PROVIDERNAME\n$ABCDESKTOP_USERID" > /data/overlay.txt
+/usr/local/bin/overlay /data/overlay.txt -f Helvetica -d 0 -o CENTER -s 100 -t 4 -o CENTER
