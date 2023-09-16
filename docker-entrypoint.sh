cbc82027267ed3f349cde7b7aba34f655af4283a
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2023-09-16 13:11:53 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,19 @@ while [ ! -S $X11SOCKET ]; do
 	sleep  0.1
 done
 echo "$ABCDESKTOP_USERNAME\n$ABCDESKTOP_PROVIDERNAME\n$ABCDESKTOP_USERID" > /data/overlay.txt
+
+# .Xauthority
+if [ ! -f ~/.Xauthority ]; then
+	log "~/.Xauthority does not exist"
+	ls -la ~ >> $STDOUT_LOGFILE
+	# create a MIT-MAGIC-COOKIE-1 entry in .Xauthority
+	if [ ! -z "$XAUTH_KEY" ]; then
+        	log "xauth add $DISPLAY MIT-MAGIC-COOKIE-1 $XAUTH_KEY"
+        	xauth add $DISPLAY MIT-MAGIC-COOKIE-1 $XAUTH_KEY >> $STDOUT_LOGFILE 2>&1
+		log "xauth add done exitcode=$?"
+	fi
+else
+	log "~/.Xauthority exists"
+fi
+
 /usr/local/bin/overlay /data/overlay.txt -f Arial -d 0 -o CENTER -s 100 -t 4 -o CENTER
