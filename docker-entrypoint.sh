266e7a3bd9c14eab10ccba7d74e44e24e576543f
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-09-16 15:17:04 +0200

    fix crash as root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-export STDOUT_LOGFILE=/tmp/x11-overlay.log
-export STDOUT_ENVLOGFILE=/tmp/x11-overlay.env.log
+export DISPLAY=${DISPLAY:-':0'}
+export USER=root
+export HOME=/root
+export LOGNAME=root
 START_TIME=$(date +%s)
 
 log() {
-echo "$(date) $1" >> $STDOUT_LOGFILE
+echo "$(date) $1"
 }
 
 X11SOCKET=/tmp/.X11-unix/X0
-echo "waiting for socket $X11SOCKET"
+log "waiting for socket $X11SOCKET"
 while [ ! -S $X11SOCKET ]; do
 	echo -n '.'
 	sleep  0.1
 done
-echo "$ABCDESKTOP_USERNAME\n$ABCDESKTOP_PROVIDERNAME\n$ABCDESKTOP_USERID" > /data/overlay.txt
+
+log "x11 is ready $X11SOCKET"
+
+echo "$ABCDESKTOP_USERNAME" >> /data/overlay.txt
+echo "$ABCDESKTOP_PROVIDERNAME" >> /data/overlay.txt
+echo "$ABCDESKTOP_USERID" >> /data/overlay.txt
+
+log "/data/overlay.txt"
+cat /data/overlay.txt
 
 # .Xauthority
 if [ ! -f ~/.Xauthority ]; then
@@ -22,11 +32,12 @@ if [ ! -f ~/.Xauthority ]; then
 	# create a MIT-MAGIC-COOKIE-1 entry in .Xauthority
 	if [ ! -z "$XAUTH_KEY" ]; then
         	log "xauth add $DISPLAY MIT-MAGIC-COOKIE-1 $XAUTH_KEY"
-        	xauth add $DISPLAY MIT-MAGIC-COOKIE-1 $XAUTH_KEY >> $STDOUT_LOGFILE 2>&1
+        	xauth add $DISPLAY MIT-MAGIC-COOKIE-1 $XAUTH_KEY 2>&1
 		log "xauth add done exitcode=$?"
 	fi
 else
 	log "~/.Xauthority exists"
 fi
+
 
 /usr/local/bin/overlay /data/overlay.txt -f Arial -d 0 -o CENTER -s 100 -t 4 -o CENTER
