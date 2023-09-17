e266eb91f6adfb502801a702287161abeb425cce
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-09-17 15:29:14 +0200

    change wait for compisitor

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ export DISPLAY=${DISPLAY:-':0'}
 export USER=root
 export HOME=/root
 export LOGNAME=root
-START_TIME=$(date +%s)
+expott LC_ALL=V
 
 log() {
 echo "$(date) $1"
@@ -13,20 +13,28 @@ echo "$(date) $1"
 X11SOCKET=/tmp/.X11-unix/X0
 log "waiting for socket $X11SOCKET"
 while [ ! -S $X11SOCKET ]; do
-	echo -n '.'
-	sleep  0.1
+	echo -n .
+	sleep  1
 done
 
 log "x11 is ready $X11SOCKET"
 
 
-echo 'run inxi -Gxx'
-
-until inxi -Gxx | grep compositor ; do 
-	echo sleep 1
-	sleep 1
+# now waiting for a compositor
+COMPOSITORS=('compiz' 'picom' 'mutter' 'kwin')
+COMPOSITOR_FOUND=0
+while [ $COMPOSITOR_FOUND -eq 0 ]
+do
+  for i in `ps -aux`; do
+    for c in ${COMPOSITORS[@]}; do
+      if [ "$i" == "$c" ]; then
+          echo "Your compositor is $i"
+          COMPOSITOR_FOUND=1
+      fi
+    done
+  done
+  sleep 1
 done
-
 
 echo "$ABCDESKTOP_USERNAME" >> /data/overlay.txt
 echo "$ABCDESKTOP_PROVIDERNAME" >> /data/overlay.txt
